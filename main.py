d4e877c92c8a96a0c5bff517c4f376294713ccbe
PonNik <nikitos1user@gmail.com>
2023-03-14 17:25:04 +0300

    min fixed

--- main.py
+++ main.py
@@ -6,25 +6,29 @@ from settings import TOKEN
 bot = TeleBot(token=TOKEN)
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['start', 'echo'])
 def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    itembtn0 = types.KeyboardButton("Moscow")
-    itembtn1 = types.KeyboardButton("Sochi")
-    itembtn2 = types.KeyboardButton("Vladivostok")
-    markup.add(itembtn0)
-    markup.add(itembtn1)
-    markup.add(itembtn2)
-    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди на сайт)", reply_markup=markup)
+    commands=['/start', '/echo']
+    if message.text.find(commands[0]) > -1:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        moscow_btn = types.KeyboardButton("Moscow")
+        sochi_btn = types.KeyboardButton("Sochi")
+        vladivostok_btn = types.KeyboardButton("Vladivostok")
+        markup.add(moscow_btn)
+        markup.add(sochi_btn)
+        markup.add(vladivostok_btn)
+        bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди на сайт)", reply_markup=markup)
+    if  message.text.find(commands[1]) > -1:
+        bot.send_message(message.chat.id, text=message.text.replace('/echo', ''))
 
 @bot.message_handler(content_types=['text'])
 def func(message):
     if(message.text == "Moscow"):
-        bot.send_message(message.chat.id, text="Погода в москве")
+        bot.send_message(message.chat.id, text="Погода в Москве")
     elif(message.text == "Sochi"):
-        bot.send_message(message.chat.id, text="Погода в москве")
+        bot.send_message(message.chat.id, text="Погода в Сочи")
     elif(message.text == "Vladivostok"):
-        bot.send_message(message.chat.id, text="Погода в москве")
+        bot.send_message(message.chat.id, text="Погода в Владивосток")
 
 
 asyncio.run(bot.polling())
