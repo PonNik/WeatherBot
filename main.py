128923cad2e23774be035565df52000a7fc4ee4e
PonNik <nikitos1user@gmail.com>
2023-03-21 02:53:57 +0300

    delete debug print

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import aiohttp
 from bs4 import BeautifulSoup as BS
 from fake_useragent import UserAgent
 import asyncio
-from settings import TOKEN, URL_WEATHER
+from settings import TOKEN
 
 bot = TeleBot(token=TOKEN)
 
-weather_list = [['Ливень', 'rain'], ['дождь', 'rain'], ['Дождь', 'rain'], ['Ясно', 'sunny'], ['Пасмурно', 'cloudy'], ['Облачно с прояснениями', 'party_cloudy'], ['снег', 'snow'], ['Снег', 'snow'], ['Гроза', 'thunderstorm']]
+weather_list = [['Ливень', 'rain'], ['дождь', 'rain'], ['Дождь', 'rain'], ['Ясно', 'sunny'], ['Пасмурно', 'cloudy'], ['Малооблачно', 'partly_cloudy'], ['Облачно с прояснениями', 'partly_cloudy'], ['снег', 'snow'], ['Снег', 'snow'], ['Гроза', 'thunderstorm']]
 
-async def parce(url):
+async def parse(url):
     HEADERS = {'User-Agent': UserAgent().random}
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=HEADERS) as response:
@@ -22,18 +22,16 @@ async def parce(url):
             info_time = info_yesterday_weather.find('time', 'time fact__time')
             info_temp = info_yesterday_weather.find('span', 'a11y-hidden')
             itog = f'{info_time.text}{info_temp.text}\n'
-            print(f'{info_time.text}{info_temp.text}')
             
             info_now_weather = soup.find('div', 'fact__temp-wrap')
             info = info_now_weather.find('a', 'link fact__basic fact__basic_size_wide day-anchor i-bem').get('aria-label')
-            print(info)
             itog += f'{info}\n'
             dop_info = soup.find('div', 'fact__props')
             veter = dop_info.find_all('span', 'a11y-hidden')
             for i in veter:
                 itog += f'{i.text}\n'
-                print(i.text)
 
+            print('parse successfully')
             return itog
 
 def get_image(weather):
@@ -61,32 +59,32 @@ def send_welcome(message):
 @bot.message_handler(content_types=['text'])
 def func(message):
     if(message.text == "Москва"):
-        weather = asyncio.run(parce('https://yandex.ru/weather/moscow'))
+        weather = asyncio.run(parse('https://yandex.ru/weather/moscow'))
         path_image = get_image(weather)
         if path_image == '':
-            bot.send_message(message.chat.id,'В Москве\n' +  weather)
+            bot.send_message(message.chat.id,'В Москве\n\n' +  weather)
         else:
-            photo1 = open(path_image, 'rb')
-            bot.send_photo(message.chat.id, photo1, 'В Москве\n' +  weather)
-            photo1.close()
+            photo = open(path_image, 'rb')
+            bot.send_photo(message.chat.id, photo, 'В Москве\n\n' +  weather)
+            photo.close()
     elif(message.text == "Сочи"):
-        weather = asyncio.run(parce('https://yandex.ru/pogoda/sochi'))
+        weather = asyncio.run(parse('https://yandex.ru/pogoda/sochi'))
         path_image = get_image(weather)
         if path_image == '':
-            bot.send_message(message.chat.id,'В Сочи\n' +  weather)
+            bot.send_message(message.chat.id,'В Сочи\n\n' +  weather)
         else:
-            photo1 = open(path_image, 'rb')
-            bot.send_photo(message.chat.id, photo1, 'В Сочи\n' +  weather)
-            photo1.close()
+            photo = open(path_image, 'rb')
+            bot.send_photo(message.chat.id, photo, 'В Сочи\n\n' +  weather)
+            photo.close()
     elif(message.text == "Владивосток"):
-        weather = asyncio.run(parce('https://yandex.ru/pogoda/vladivostok'))
+        weather = asyncio.run(parse('https://yandex.ru/pogoda/vladivostok'))
         path_image = get_image(weather)
         if path_image == '':
-            bot.send_message(message.chat.id,'Во Владивостоке\n' +  weather)
+            bot.send_message(message.chat.id,'Во Владивостоке\n\n' +  weather)
         else:
-            photo1 = open(path_image, 'rb')
-            bot.send_photo(message.chat.id, photo1, 'Во Владивостоке\n' +  weather)
-            photo1.close()
+            photo = open(path_image, 'rb')
+            bot.send_photo(message.chat.id, photo, 'Во Владивостоке\n\n' +  weather)
+            photo.close()
     else:
         bot.send_message(message.chat.id, '')
 
